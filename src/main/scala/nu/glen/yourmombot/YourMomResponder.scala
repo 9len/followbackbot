c56c24ff3c92202135ef3b3b30a96e46af8e8c3a
Glen D Sanford <glen@twitter.com>
2012-08-14 17:11:21 -0700

    Really fix rate limiter logging

--- src/main/scala/nu/glen/yourmombot/YourMomResponder.scala
+++ src/main/scala/nu/glen/yourmombot/YourMomResponder.scala
@@ -3,9 +3,11 @@ package nu.glen.yourmombot
 import nu.glen.followbackbot.{GerrundKeywordPrefixResponder, PastTenseKeywordPrefixResponder}
 
 object YourMomGerrundResponder extends GerrundKeywordPrefixResponder {
+  override def name = "YourMomGerrundResponder"
   override def combine(gerrund: String, rest: String) = "Your mom's %s%s".format(gerrund, rest)
 }
 
 object YourMomPastTenseResponder extends PastTenseKeywordPrefixResponder {
+  override def name = "YourMomPastTenseResponder"
   override def combine(verb: String, rest: String) = "Your mom %s%s".format(verb, rest)
 }
